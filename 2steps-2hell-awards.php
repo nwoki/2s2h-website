65c16d82caad22dcbd1671d5ba82b6fe0a96edc3
Francesco <n3m3sis@email.it>
2010-09-21 00:08:49 +0200

    awards page

--- 2steps-2hell-awards.php
+++ 2steps-2hell-awards.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-		$_SESSION["page"] = "awards"; 
+		$_SESSION["page"] = "awards";
     require( 'functions/functions.php' );
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -10,37 +10,36 @@
     <meta name="description" content="<?php echo $description[$_SESSION["page"]]; ?>" />
     <title><?php echo $title[$_SESSION["page"]]; ?></title>
     <link rel="stylesheet" href="css/style.css" type="text/css" />
-    
 
-</head> 
+
+</head>
 <body>
     <div id="header">
       <a href="index.php" title="2step 2hell - Noleggio Server - BanBot"><img alt="" src="img/spacer.gif" width="800" height="235" /></a>
     </div>
-        
+
     <div class="separate">
 		  <div id="menu-top"></div>
       <div id="menu-center"><?php menuPages( $_SESSION["page"] ) ?></div>
       <div id="menu-bottom"></div>
-    </div>  
-	
-    	
+    </div>
+
+
     <div id="content">
       <div id="padding">
-        <h2>AWARDS</h2>
-            
-        
-    
+        <h2 align="center">AWARDS</h2>
+
+
+
         <center>
-            <img src="img/awards/esl/DD3.jpg" >
+            <img src="imghold/awards/esl/DD3.jpg" >
         </center>
 
       </div>
     </div>
 
-    
     <div class="separate sfondo-footer"><?php bottomPageInfo(); ?></div>
-    
-    
+
+
 </body>
 </html>
