2e12eb1247cd44906fae88cd85436f166d986425
Simone <skyled@alice.it>
2010-09-24 20:49:37 +0200

    Sistemata la bagina del BanBot.

--- 2steps-2hell-banbot.php
+++ 2steps-2hell-banbot.php
@@ -28,13 +28,38 @@
       <div id="padding">
         <h1 class="bot"><span>BanBot</span></h1>
         <p class="center"><img src="img/banbot.jpg" align="center" alt="BanBot"></p>
-        <p class="center">Un bot fatto in C++ che mira ad essere semplice e ad occupare poche risorse della macchina che lo hosta limitando ( addirittura eliminando ) "lag" non voluti del server spesso provocati dai bot.</p>
-        <p class="center">Controlla il gioco di URBAN TERROR 4.1 e utilizza un database SQLite3 per contenere i suoi dati ( la libreria SQLite3 &egrave intergata nel bot, quindi non &egrave richiesto che sia installata sulla macchina ).</p>
-        <p class="center">Controlla periodicamente per giocatori bannati e li elimina automaticamente dal server evitando il loro rientro nel futuro.</p>  
-        <p class="center"><a href="banbotReadme.txt">Readme</a> completo del bot</p>
-        <p class="center">Aiutaci a migliorare il bot indicandoci bug o modi per migliorarlo che secondo voi sono utili aprendo un ticket sul nostro <a href="bugtracker/index.php">bugtracker</a></p>
-        <p class="center"><a href="banbotDownloads/banbot_1.1.zip"><img src="imghold/download.png" alt=""></a></p> 
-        <p class="center"><a href="banbotDownloads/banbot_1.1.zip">Banbot_ver1.1</a> - <a href="banbotDownloads/ChangeLog_ver1.1.txt" target="_blank">(ChangeLog)</a></p>
+<!-- italiano -->        
+        <div class="col1" lang="it">
+        	<h1 class="italiano"></h1>
+		    <p class="center">Un bot che mira ad essere semplice e ad occupare poche risorse della macchina che lo hosta evitando quindi di provocare "lag" non voluti del server.</p>
+		    <p class="center">Fatto per controllare il gioco di URBAN TERROR 4.1, integra ed utilizza la libreria SQLite3 per contenere i suoi dati: nonstante questo &egrave completamente indipendente, non necessita di alcun software preinstallato.</p>
+		    <p class="center">Offre molti strumenti utili per gli amministratori dei server, per esempio ban permanenti e un anti-cheat.</p>  
+		    <p class="center">Per maggiori informazioni, leggete la documentazione.</p>
+		    <p class="center">Aiutaci a migliorare il bot indicandoci bug o dandoci dei suggerimenti: apri un ticket sul nostro <a href="bugtracker/index.php">BugTracker</a>.</p>
+		    <p class="center"><a href="banbotDownloads/banbot_1.1.zip"><img src="imghold/download.png" alt=""></a></p> 
+		    <p class="center"><a href="banbotDownloads/banbot_1.1.zip">Banbot_ver1.1(ITA)</a> - <a href="banbotDownloads/ChangeLog_ver1.1.txt" target="_blank">(ChangeLog)</a></p>
+		</div>
+		
+		<div class="separatore"> </div>
+<!-- inglese -->
+		<div class="col2" lang="en">
+			<h1 class="inglese"></h1>
+		    <p class="center">A bot that is designed to be simple and to use few resources of the machine thus to avoid causing unwanted "lags" on server.</p>
+		    <br/>
+		    <p class="center">Made to control the game Urban Terror 4.1, it integrates and uses the SQLite3 library to contain your data: spite this is completely independent, requires no preinstalled software.</p>
+		    <br/>
+		    <p class="center">It offers many useful tools for server administrators, for example permanent ban and an anti-cheat.</p>  
+		    <p class="center">For more information, read the documentation.</p>
+		    <p class="center">Help us improve the bot indicating bug or give us suggestions: open a ticket on our <a href="bugtracker/index.php">BugTracker</a>.</p>
+		    <p class="center"><a href="banbotDownloads/banbot_1.1.zip"><img src="imghold/download.png" alt=""></a></p> 
+		    <p class="center"><a href="banbotDownloads/banbot_1.1.zip">Banbot_ver1.1(ENG)</a> - <a href="banbotDownloads/ChangeLog_ver1.1.txt" target="_blank">(ChangeLog)</a></p>
+		</div>
+		
+		<div id="fineBot">
+			<p class="center"><a href="banbotReadme.txt"><img src="imghold/download.png" alt=""></a></p> 
+			<p class="center"><a href="banbotReadme.txt">documentazione - documentation</a></p>
+		</div>
+		
       </div>
     </div>
 
