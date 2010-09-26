ad26d82f23b783b3da19072448398052b2ed42bc
Simone <skyled@alice.it>
2010-09-27 00:35:03 +0200

    Modifiche alla pagina BanBot per il listing automatico. Da finire.

--- 2steps-2hell-banbot.php
+++ 2steps-2hell-banbot.php
@@ -2,6 +2,42 @@
     session_start();
 		$_SESSION["page"] = "bot";
     require( 'functions/functions.php' );
+    
+	// open the directory 
+	$myDirectory = opendir("BanBot/last_release/");
+
+	// get each entry
+	while($entryName = readdir($myDirectory)) {
+		$dirArray[] = $entryName;
+	}
+
+	// close directory
+	closedir($myDirectory);
+
+	//	count elements in array
+	$indexCount	= count($dirArray);
+
+	// sort 'em
+	sort($dirArray);
+
+	// print 'em
+	print("<TABLE border=1 cellpadding=5 cellspacing=0 class=whitelinks>\n");
+	print("<TR><TH>Filename</TH><th>Filetype</th><th>Filesize</th></TR>\n");
+	// loop through the array of files and print them all
+	for($index=0; $index < $indexCount; $index++) {
+		    if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
+			print("<TR><TD><a href=\"$dirArray[$index]\">$dirArray[$index]</a></td>");
+			print("<td>");
+			print(filetype($dirArray[$index]));
+			print("</td>");
+			print("<td>");
+			print(filesize($dirArray[$index]));
+			print("</td>");
+			print("</TR>\n");
+		}
+	}
+	print("</TABLE>\n");
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -49,6 +85,8 @@
 		    <p class="center">Riassumendo, se ne sta buono e silenzioso, tranne in caso di bisogno, e tiene i vostri server puliti.</p>
 		    <p class="center">Per maggiori informazioni, leggete la documentazione.</p>
 		    <p class="center">Aiutaci a migliorare BanBot indicandoci bug o dandoci dei suggerimenti: apri un ticket sul nostro <a href="bugtracker/index.php">BugTracker</a>.</p>
+		    
+
 		    <p class="center"><a href="banbotDownloads/banbot_1.1.zip"><img src="imghold/download.png" alt=""></a></p>
 		    <p class="center"><a href="banbotDownloads/banbot_1.1.zip">Banbot_ver1.1(ITA)</a> - <a href="banbotDownloads/ChangeLog_ver1.1.txt" target="_blank">(ChangeLog)</a></p>
 		    <br/>
