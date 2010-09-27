36b0b151f1bdeb782d78ba9fe77cae8e53068fba
Rambo <matteo.furletti@gmail.com>
2010-09-27 19:16:32 +0200

    Aggiunto Slide Panel lato backend
    Aggiunto lo slidepanel alle pagine modnews.php e modadmins.php

--- admin/modnews.php
+++ admin/modnews.php
@@ -1,12 +1,12 @@
 <?php
     ob_start();
     session_start();
-		if ( !isset($_SESSION['status']) || $_SESSION['status']!="admin" )
+    if ( !isset($_SESSION['status']) || $_SESSION['status']!="admin" )
       header('location: ../index.php');
     require( '../functions/functions.php' );
     $_SESSION["page"] = "admin";
     ob_end_flush();
-		//error_reporting( E_ALL );
+    //error_reporting( E_ALL );
     //ini_set( 'display_errors', '1' );
     //$test = new WebClass(); /* used to check if database is setup right */
 ?>
@@ -16,14 +16,40 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <meta name="description" content="<?php echo $description[$_SESSION["page"]]; ?>" />
-        <link rel="stylesheet" href="../css/style.css" type="text/css" />
+        <title><?php echo $title[$_SESSION["page"]]; ?></title>
+        <script src="../js/jquery-1.4.2.min.js" type="text/javascript"></script>
+        <script src="../js/login.js" type="text/javascript"></script>
+        <?php if ( isset($_SESSION['status']) && $_SESSION['status'] == 'admin') {?>
+        <script src="../js/slide-admin.js" type="text/javascript"></script>
+        <?php } else {?>
+        <script src="../js/slide.js" type="text/javascript"></script>
+        <?php }?>
+        <link href="../css/style.css" type="text/css" rel="stylesheet"/>
+        <link href="../css/slide.css" type="text/css" rel="stylesheet" media="screen" />
     </head>
 
-    <title>2Steps2Hell - ModNews</title>
-
-    </head>
-
-    <body>
+    <body>    
+    <!-- Panel -->
+    <div id="toppanel">
+      <div id="panel">
+        <div class="content clearfix">
+          <div class="left all">
+            <?php restrictedArea( $_SESSION['status'] ) ?>
+          </div>
+        </div>
+      </div> 
+      <!-- The tab on top -->  
+      <div class="tab">
+        <ul class="login">
+          <li class="left">&nbsp;</li>
+          <li id="toggle">
+            <a id="open" class="open" href="#">Admin Area</a>
+            <a id="close" class="close" href="#">Close</a>      
+          </li>
+          <li class="right">&nbsp;</li>
+        </ul> 
+      </div> <!-- / top -->
+    </div> <!--panel -->
 
         <div id="header">
             <a href="../index.php"><img alt="" src="../img/spacer.gif" width="800" height="235" /></a>
@@ -32,9 +58,7 @@
         <div class="separate">
             <div id="menu-top" />
             <div id="menu-center">
-                <?php
-                    menuPages( $_SESSION["page"] );
-                ?>
+                <?php menuPages( $_SESSION["page"] );?>
             </div>
             <div id="menu-bottom" />
         </div>
@@ -61,13 +85,7 @@
             ?>
         </div>
 
-        <div class="separate sfondo-footer">
-            <?php
-                bottomPageInfo();
-            ?>
-        </div>
-
-
+        <div class="separate sfondo-footer"><?php bottomPageInfo();?></div>
 
 </body>
 </html>
