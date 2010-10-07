fa70131aff7e343043327485623e2bb806b7375c
Simone <skyled@alice.it>
2010-10-07 04:14:08 +0200

    Piccola correzione al pulsante per le donazioni.

--- 2steps-2hell-banbot.php
+++ 2steps-2hell-banbot.php
@@ -342,6 +342,7 @@
         	<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 			<input type="hidden" name="cmd" value="_s-xclick">
 			<input type="hidden" name="hosted_button_id" value="E6M9R7GX33PNW">
+			<input type="hidden" name="item_name" value="2Steps2Hell UrT Clan">
 			<input type="image" src="https://www.paypal.com/it_IT/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - Il sistema di pagamento online più facile e sicuro!">
 			<img alt="" border="0" src="https://www.paypal.com/it_IT/i/scr/pixel.gif" width="1" height="1">
 			</form>
@@ -353,6 +354,7 @@
 		    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 			<input type="hidden" name="cmd" value="_s-xclick">
 			<input type="hidden" name="hosted_button_id" value="E6M9R7GX33PNW">
+			<input type="hidden" name="item_name" value="2Steps2Hell UrT Clan">
 			<input type="image" src="https://www.paypal.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
 			<img alt="" border="0" src="https://www.paypal.com/it_IT/i/scr/pixel.gif" width="1" height="1">
 			</form>
