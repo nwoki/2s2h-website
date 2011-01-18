e2760cf651b6039098b0815253813646c0a06729
Francesco <francesco.nwokeka@gmail.com>
2011-01-18 02:33:10 +0100

    aggiornato git con modifiche portate al sito

--- 2steps-2hell-banbot.php
+++ 2steps-2hell-banbot.php
@@ -58,6 +58,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta name="description" content="<?php echo $description[$_SESSION["page"]]; ?>" />
+    <meta name="keywords" content="server rent, server renting, server hosting, server host, urban terror, urt, urt server hosting, server hosting urt, BanBot, anticheat" />
     <title><?php echo $title[$_SESSION["page"]]; ?></title>
 		<script src="js/jquery-1.4.2.min.js" type="text/javascript" ></script>
 		<script src="js/jquery-ui-1.8.4.custom.min.js" type="text/javascript" ></script>
@@ -78,6 +79,22 @@
     <link rel="stylesheet" href="css/style.css" type="text/css" />
     <link rel="stylesheet" href="css/BanBot.css" type="text/css" />
     <link rel="stylesheet" href="css/slide.css" type="text/css" media="screen" />
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19353394-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 
 </head>
 <body>
