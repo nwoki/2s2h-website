15daeb668b4d3c01fe4085f625573fbbe7d4dbcb
zamy <skyled@alice.it>
2011-07-18 15:47:17 +0200

    Adattata al nuovo sistema anche la pagina "contact us".

--- 2steps-2hell-contact-us.php
+++ 2steps-2hell-contact-us.php
@@ -1,7 +1,10 @@
 <?php
+    ob_start();
     session_start();
-    $_SESSION["page"] = "contact";
     require( 'functions/functions.php' );
+    checkLanguage();
+    ob_end_flush();
+    $_SESSION["page"] = "contact";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -53,62 +56,84 @@
       <div id="menu-bottom"></div>
     </div>
 
+<?php
+	if (languageSelection())
+	{
+?>
     <div id="content">
       <div id="padding">
         <h1 class="contact"><span>Contact Us</span></h1>
 
-        <div class="box-alto">
-          <div class="box-basso">
-    				<div class="box-medio">
-      				<div class="box-testo">
+		<div class="box-alto">
+			<div class="box-basso">
+				<div class="box-medio">
+					<div class="box-testo">
+						<?php if ($_SESSION["language"]=="it") { ?>
+						<h2><span class="irc">irc-icon</span>IRC: Puoi trovarci tutti sul canale <em>#2s2h</strong></em> @ "<em>irc.quakenet.org</em>".
+						<h4>amministratori + programmatori ( noleggio server, bugs, ecc )</h4>
+						<ul>
+						<li><strong>N3m3s1s</strong> -<span class="email">email</span> n3m3s1s<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com (Noleggio server & admin, BanBot, WebSite)</li>
+						<li><strong>Zamy</strong> -<span class="email">email</span> zamy<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com (Noleggio server & admin, BanBot, WebSite)</li>
+						<li><strong>Rambo</strong> -<span class="email">email</span> urt.ks.rambo<span class="symb">(at)</span>gmail<span class="symb">(dot)</span>com (WebSite)</li>
+						</ul>
+						<?php }else { ?>
+						<h2><span class="irc">irc-icon</span>IRC: You can find all of us on <em>#2s2h</strong></em> @ "<em>irc.quakenet.org</em>".
+						<h4>system admins + devs ( server renting, bugs etc )</h4>
+						<ul>
+						<li><strong>N3m3s1s</strong> -<span class="email">email</span> n3m3s1s<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com (Server renting & admin, BanBot, WebSite)</li>
+						<li><strong>Zamy</strong> -<span class="email">email</span> zamy<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com (Server renting & admin, BanBot, WebSite)</li>
+						<li><strong>Rambo</strong> -<span class="email">email</span> urt.ks.rambo<span class="symb">(at)</span>gmail<span class="symb">(dot)</span>com (WebSite)</li>
+						</ul>
+						<?php } ?>
+					</div>
+				</div>
+			</div>
+		</div>
 
-                <h2><span class="irc">irc-icon</span>IRC: You can find all of us on "<em>irc.quakenet.org</em>" @ <em>#2s2h</strong>.</em>
-      					<h4>system admins + devs( server renting, bugs ecc )</h4>
+		<div class="box-alto">
+			<div class="box-basso">
+				<div class="box-medio">
+					<div class="box-testo">
+						<?php if ($_SESSION["language"]=="it") { ?>
+						<h2><span class="ts">ts2</span> admins di teamspeak </h2>
+						<ul>
+						<li><strong>Heresyarch/Tsujigiri</strong> - <span class="email"></span>heresy<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com</li></li>
+						<li><strong>Revan</strong> - <span class="email"></span>revan<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com</li>
+						</ul>
+						<?php }else { ?>
+						<h2><span class="ts">ts2</span> teamspeak admins</h2>
+						<ul>
+						<li><strong>Heresyarch/Tsujigiri</strong> - <span class="email"></span>heresy<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com</li></li>
+						<li><strong>Revan</strong> - <span class="email"></span>revan<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com</li>
+						</ul>
+						<?php } ?>
+					</div>
+				</div>
+			</div>
+		</div>
 
-                <ul>
-                  <li><strong>N3m3s1s</strong> -<span class="email">email</span> n3m3s1s<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com</li>
-                  <li>(Server renting & admin, BanBot, WebSite)</li>
-                  <li><strong>Zamy</strong> -<span class="email">email</span> zamy<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com</li>
-                  <li>(Server renting & admin, BanBot, WebSite)</li>
-                  <li><strong>Rambo</strong> -<span class="email">email</span> urt.ks.rambo<span class="symb">(at)</span>gmail<span class="symb">(dot)</span>com (WebSite)</li>
-                </ul>
-
-      				</div>
-    				</div>
-  				</div>
-			  </div>
-
-        <div class="box-alto">
-          <div class="box-basso">
-    				<div class="box-medio">
-      				<div class="box-testo">
-
-                 <h2><span class="ts">ts2</span> teamspeak admins</h2>
-                 <ul>
-                 <li><strong>Heresyarch/Tsujigiri</strong> - <span class="email"></span>heresy<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com</li></li>
-                  <li><strong>Revan</strong> - <span class="email"></span>revan<span class="symb">(at)</span>2s2h<span class="symb">(dot)</span>com</li>
-                </ul>
-
-      				</div>
-    				</div>
-  				</div>
-			  </div>
-
-       <div class="box-alto">
-          <div class="box-basso">
-    				<div class="box-medio">
-      				<div class="box-testo">
-                <h2>BanBot admins 2s2h server</h2>
-                <h4>See "IRC" above. All the opped people are also admins on our servers BanBot</h4>
-      				</div>
-    				</div>
-  				</div>
-			  </div>
-
-     </div>
+		<div class="box-alto">
+			<div class="box-basso">
+				<div class="box-medio">
+					<div class="box-testo">
+						<?php if ($_SESSION["language"]=="it") { ?>
+				        <h2>BanBot admins dei server 2s2h</h2>
+				        <h4>Guardate "IRC" qua sopra. Tutte le persone che sono amministratori del canale sono anche amministratori dei server.</h4>
+				        <?php }else { ?>
+				        <h2>BanBot admins 2s2h server</h2>
+				        <h4>See "IRC" above. All the opped people are also admins on our servers BanBot</h4>
+				        <?php } ?>
+	  				</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
   </div>
-  <div class="separate sfondo-footer"><?php bottomPageInfo(); ?></div>
+<?php
+	}
+?>
+<?php bottomPageInfo(); ?>
 
 
 </body>
